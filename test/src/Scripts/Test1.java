1b9a4741b4dd1fe0b8b9f7934d86bed03c9bcee1
apple <apple@apples-mbp>
2024-06-17 13:56:31 +0530

    First commit

---
+++ test/src/Scripts/Test1.java
@@ -0,0 +1,23 @@
+package Scripts;
+
+public class Test1 {
+
+	public static void main(String[] args) {
+
+		
+		String hungry = "Yes";
+		
+		if(hungry == "Yes" || hungry == "Y" || hungry == "y")
+		{
+			System.out.println("Eat food");
+		}
+		else
+		{
+			System.out.println("Go work");
+		}
+			
+
+
+	}
+
+}
