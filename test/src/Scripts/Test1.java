751c7070c8fdac614a84958bbb565405c87cfd5f
apple <apple@apples-mbp>
2024-06-17 14:02:18 +0530

    Commit with changes

--- test/src/Scripts/Test1.java
+++ test/src/Scripts/Test1.java
@@ -5,13 +5,14 @@ public class Test1 {
 	public static void main(String[] args) {
 
 		
-		String hungry = "Yes";
+		String hungry = "No";
 		
 		if(hungry == "Yes" || hungry == "Y" || hungry == "y")
 		{
 			System.out.println("Eat food");
 		}
 		else
+		if(hungry == "No")
 		{
 			System.out.println("Go work");
 		}
